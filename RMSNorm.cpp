64b2e97f49ca1051f1d278268b41f57daa5fafcb
Dirk Groeneveld <groeneveld@gmail.com>
2025-10-08 00:15:01 -0700

    Move RMSNorm implementation into the cpp

--- RMSNorm.cpp
+++ RMSNorm.cpp
@@ -5,3 +5,9 @@
 RMSNorm::RMSNorm(const std::string& filename) {
     m_weight = xt::load_npy<float>(filename);
 }
+
+xt::xtensor<float, 1> RMSNorm::forward(const xt::xtensor<float, 1>& input) {
+    const auto rms = xt::sqrt(xt::mean(xt::square(input)) + eps);
+    const auto x = input / rms;
+    return x * m_weight;
+}
