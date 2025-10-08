5c99c58643c29d16194c93ab181f609c406b4a63
Dirk Groeneveld <groeneveld@gmail.com>
2025-10-08 00:15:29 -0700

    Move OlmoMlp implementation into the cpp

--- OlmoMlp.h
+++ OlmoMlp.h
@@ -9,14 +9,7 @@ class OlmoMlp {
 public:
     OlmoMlp(const std::string& folder, unsigned int index);
 
-    xt::xtensor<float, 1> forward(const auto& input) {
-        const auto projected = xt::linalg::dot(input, m_upProjection);
-        const auto gate = xt::linalg::dot(input, m_gateProjection);
-        const auto sigmoid = 1.0 / (1.0 + xt::exp(-gate));
-        const auto silu = gate * sigmoid;
-        const auto result = projected * silu;
-        return xt::linalg::dot(result, m_downProjection);
-    }
+    xt::xtensor<float, 1> forward(const xt::xtensor<float, 1>& input);
 
 private:
     xt::xtensor<float, 2> m_upProjection;
