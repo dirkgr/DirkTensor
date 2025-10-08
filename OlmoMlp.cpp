e1b5f4e49312f4a723875c1ddc9234924b515bf7
Dirk Groeneveld <groeneveld@gmail.com>
2025-10-08 00:15:29 -0700

    Move OlmoMlp implementation into the cpp

--- OlmoMlp.cpp
+++ OlmoMlp.cpp
@@ -17,3 +17,12 @@ OlmoMlp::OlmoMlp(const std::string& folder, const unsigned int index) {
             xt::load_npy<float>(
                 std::format("{}/model.layers.{}.mlp.down_proj.weight.npy", folder, index)));
 }
+
+xt::xtensor<float, 1> OlmoMlp::forward(const xt::xtensor<float, 1>& input) {
+    const auto projected = xt::linalg::dot(input, m_upProjection);
+    const auto gate = xt::linalg::dot(input, m_gateProjection);
+    const auto sigmoid = 1.0 / (1.0 + xt::exp(-gate));
+    const auto silu = gate * sigmoid;
+    const auto result = projected * silu;
+    return xt::linalg::dot(result, m_downProjection);
+}
