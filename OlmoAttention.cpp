d8b4bce15913517cd7cbf9337471c4b7152bf34d
Dirk Groeneveld <groeneveld@gmail.com>
2025-10-09 00:51:08 -0700

    Makes the first token work!

--- OlmoAttention.cpp
+++ OlmoAttention.cpp
@@ -7,10 +7,10 @@ OlmoAttention::OlmoAttention(const std::string& folder, const unsigned int index
     m_qNorm(std::format("{}/model.layers.{}.self_attn.q_norm.weight.npy", folder, index)),
     m_kNorm(std::format("{}/model.layers.{}.self_attn.k_norm.weight.npy", folder, index))
 {
-    m_qProj = xt::load_npy<float>(std::format("{}/model.layers.{}.self_attn.q_proj.weight.npy", folder, index));
-    m_kProj = xt::load_npy<float>(std::format("{}/model.layers.{}.self_attn.k_proj.weight.npy", folder, index));
-    m_vProj = xt::load_npy<float>(std::format("{}/model.layers.{}.self_attn.v_proj.weight.npy", folder, index));
-    m_oProj = xt::load_npy<float>(std::format("{}/model.layers.{}.self_attn.o_proj.weight.npy", folder, index));
+    m_qProj = xt::transpose(xt::load_npy<float>(std::format("{}/model.layers.{}.self_attn.q_proj.weight.npy", folder, index)));
+    m_kProj = xt::transpose(xt::load_npy<float>(std::format("{}/model.layers.{}.self_attn.k_proj.weight.npy", folder, index)));
+    m_vProj = xt::transpose(xt::load_npy<float>(std::format("{}/model.layers.{}.self_attn.v_proj.weight.npy", folder, index)));
+    m_oProj = xt::transpose(xt::load_npy<float>(std::format("{}/model.layers.{}.self_attn.o_proj.weight.npy", folder, index)));
 
     // kv cache
     m_kCache = xt::empty<float>({seq_len, n_heads, head_dim});
