792f168388dfe678f9611eac2d1367476cb2a421
Dirk Groeneveld <groeneveld@gmail.com>
2025-10-14 00:12:26 -0700

    Massive perf

--- OlmoModel.cpp
+++ OlmoModel.cpp
@@ -8,7 +8,7 @@ OlmoModel::OlmoModel(const std::string& folder) : m_norm(folder + "/model.norm.w
     assert(m_embeddings.shape().size() == 2);
     assert(m_embeddings.shape()[1] == d_model);
 
-    m_lmHead = xt::transpose(xt::load_npy<float>(folder + "/lm_head.weight.npy"));
+    m_lmHead = xt::load_npy<float>(folder + "/lm_head.weight.npy");
 
     for(size_t i = 0; i < n_layers; i++)
         m_blocks[i] = std::make_unique<OlmoBlock>(folder, i);
@@ -26,5 +26,5 @@ xt::xtensor<float, 1> OlmoModel::forward(const uint32_t token) {
     x = m_norm.forward(x);
 
     // LM Head
-    return xt::linalg::dot(x, m_lmHead);
+    return xt::linalg::dot(m_lmHead, x);
 }
