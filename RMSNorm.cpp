6a7e57437f294ee0b928f9c57f1fc85a6a05e8e9
Dirk Groeneveld <groeneveld@gmail.com>
2025-10-16 22:27:04 -0700

    Revert "Add noalias and pre-allocated buffers - SLOWER: 53.09s vs 49.48s"
    This reverts commit a4adf724e2a925a30a5f0cf53659e72984ec625b.

--- RMSNorm.cpp
+++ RMSNorm.cpp
@@ -2,15 +2,13 @@
 
 #include <xtensor/core/xmath.hpp>
 #include <xtensor/io/xnpy.hpp>
-#include <xtensor/core/xnoalias.hpp>
 
 RMSNorm::RMSNorm(const std::string& filename) {
     m_weight = xt::load_npy<float>(filename);
-    m_xBuffer = xt::empty<float>(m_weight.shape());
 }
 
 xt::xtensor<float, 1> RMSNorm::forward(const xt::xtensor<float, 1>& input) {
     const auto rms = xt::sqrt(xt::mean(xt::square(input)) + eps);
-    xt::noalias(m_xBuffer) = input / rms;
-    return m_xBuffer * m_weight;
+    const auto x = input / rms;
+    return x * m_weight;
 }
