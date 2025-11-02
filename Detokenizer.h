81e27429b47ad2a474bd4f7bc948a04672269ad8
Dirk Groeneveld <groeneveld@gmail.com>
2025-11-02 12:50:05 -0800

    Detokenizer needs to know about padding tokens

--- Detokenizer.h
+++ Detokenizer.h
@@ -9,7 +9,9 @@ public:
     explicit Detokenizer(const std::string& vocab_file);
 
     std::string decode(uint32_t token_id) const;
+    uint32_t get_pad_token_id() const { return m_padTokenId; }
 
 private:
     std::vector<std::string> m_vocab;
+    uint32_t m_padTokenId;
 };
